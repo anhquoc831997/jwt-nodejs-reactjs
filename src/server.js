548f8b6bf724d6a39fdcc00f77a9f56c226ee707
Anh Quốc <quoctran@Quocs-MacBook-Pro.local>
2023-09-09 21:48:01 +0700

    init project

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import configViewEngine from "./configs/viewEngine";
+import initWebRoutes from "./routes/web";
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+//config view engine
+configViewEngine(app);
+
+//init web routes
+initWebRoutes(app);
+
+app.listen(PORT, () => {
+    console.log("jwt backend is running on the port = " + PORT);
+});
