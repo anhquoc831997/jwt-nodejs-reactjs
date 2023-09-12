c219b2ff72709239a9d5b1e27b153c327cbb750c
Anh Quốc <quoctran@Quocs-MacBook-Pro.local>
2023-09-12 23:28:25 +0700

    orm demo

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
 import bodyParser from 'body-parser';
+import connection from './config/connectDB';
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 
@@ -13,6 +14,9 @@ configViewEngine(app);
 //config body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//test connection db
+connection();
 //init web routes
 initWebRoutes(app);
 
