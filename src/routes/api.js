3b5906953e1edb7dbf08aaac2b3b0c5c81369ce7
Anh Quốc <quoctran@Quocs-MacBook-Pro.local>
2023-09-25 22:49:03 +0700

    config api

---
+++ src/routes/api.js
@@ -0,0 +1,16 @@
+import express from "express";
+import apiController from "../controller/apiController"
+const router = express.Router();
+
+/**
+ * 
+ * @param {*} app - express app
+ */
+const initApiRoutes = (app) => {
+    //rest api
+    router.get("/test-api", apiController.testApi);
+    router.post("/register", apiController.handleRegister)
+    return app.use("/api/v1/", router);
+}
+
+export default initApiRoutes;
--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
+import initApiRoutes from "./routes/api";
 import bodyParser from 'body-parser';
 import connection from './config/connectDB';
+import configCors from './config/cors';
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
-
 const app = express();
+
+//config cors
+configCors(app);
 
 //config view engine
 configViewEngine(app);
@@ -19,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 connection();
 //init web routes
 initWebRoutes(app);
-
+initApiRoutes(app);
 app.listen(PORT, () => {
     console.log("jwt backend is running on the port = " + PORT);
 });
